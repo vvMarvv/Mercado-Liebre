88cdb8342c983438ac082ef3ad655085d930f653
Maria <mmariabelenn@gmail.com>
2021-11-04 11:25:25 -0300

    nuevo

---
+++ app.js
@@ -0,0 +1,15 @@
+let express = require('express') //requiero express
+let app = express(); //Ejecuto express 
+const PORT = 3000;
+const path = require('path')
+
+app.use(express("static"));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+
+app.listen(PORT, () => console.log(`servidor levantado en el puerto${PORT}
+http://localhost:${PORT}
+`))
