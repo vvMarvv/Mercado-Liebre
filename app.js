92c5863f6a25548359703dc206258377897ad5cf
Maria B Mamani <mmariabelenn@gmail.com>
2022-04-10 12:46:00 -0300

    desafios

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-let express = require('express') //requiero express
-let app = express(); //Ejecuto express 
-const PORT = 3000;
-const path = require('path')
+const express = require("express")
+let app = express()
+const PORT = 3000
+let path = require("path")
 
-app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+app.use(express.static("public"))
+
+
+// RUTAS
+app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
 })
 
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
 
-app.listen(PORT, () => console.log(`servidor levantado en el puerto${PORT}
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+
+// SERVIDOR
+app.listen(PORT, () => console.log(`
+Servidor escuchando en el puerto ${PORT}
 http://localhost:${PORT}
 `))
